12db0f540fdf8072f05f0347d37047e49236f68c
Kevin Nuñez <101608595+jumendez16@users.noreply.github.com>
2022-11-07 08:25:15 -0500

    a

--- config.js
+++ config.js
@@ -14,12 +14,12 @@ import {
 } from '@env';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD22ofBgkcbLAKTy7dJ8F1Vdw3NLYxWE7w',
-  authDomain: 'baik-c576e.firebaseapp.com',
-  projectId: 'baik-c576e',
-  storageBucket: 'baik-c576e.appspot.com',
-  messagingSenderId: '1065140443176',
-  appId: '1:1065140443176:web:94f942e16c16834a19bd1d',
+  apiKey: firebaseApikey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectI,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
 };
 
 if (!firebase.apps.length) {
